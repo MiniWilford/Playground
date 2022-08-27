66e7452be68360248cc134e55409c7c06a63ed79
MiniWilford <ktbtut@gmail.com>
2022-08-26 20:25:11 -0400

    Practicing object oriented concepts

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+
+/*
+
+    All code is used for educational purposes only
+
+ */
 public class Main {
+    private static int milesPerGallon = 20;
+    private static int odometer = 0;
+    private static double gallonsOfGas = 10.0;
+
+    int milesDriven = 100;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello!");
+
+        int milesDriven = 100;
+        drive(milesDriven);
+
+        milesDriven = 60;
+        drive(milesDriven);
+
+        drive(90);
+    }
+
+    private static void drive(int milesDriven) {
+        gallonsOfGas = gallonsOfGas - (milesDriven /milesPerGallon); // same as -= (milesdriven/milespergallon)
+        odometer = odometer + milesDriven; //same as += shorthand method
+
+        System.out.println("Odometer: " + odometer);
+        System.out.println("Gallons of gas: " + gallonsOfGas);
     }
 }
